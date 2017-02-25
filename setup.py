0df6c268191c7b51831fb5d3e9310f72e041816e
Taylor J. Meek <taylor@cloudbolt.io>
2017-02-24 23:05:48 -0800

    0.1.1

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from distutils.core import setup
+
+VERSION = '0.1.1'
+
 setup(
     name='githubclient',
     packages=['githubclient'],
-    version='0.1',
+    version='0.1.1',
     description='A Python github client.',
     author='Taylor J. Meek, CloudBolt Software',
     author_email='taylor+pypi@cloudbolt.io',
     url='https://github.com/CloudBoltSoftware/githubclient',
-    download_url='https://github.com/CloudBoltSoftware/githubclient/tarball/0.1',
+    download_url='https://github.com/CloudBoltSoftware/githubclient/tarball/0.1.1',
     keywords=['github', 'api', 'rest', 'client'],
     license='MIT',
     long_description=open('README').read(),
